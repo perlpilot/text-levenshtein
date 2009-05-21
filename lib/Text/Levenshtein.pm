77155ffaf48f2e63c214ae43846e6f4e3c395416
Jonathan Scott Duff <duff@pobox.com>
2009-05-21 17:40:40 -0500

    clean up code

--- lib/Text/Levenshtein.pm
+++ lib/Text/Levenshtein.pm
@@ -1,43 +1,43 @@
 module Text::Levenshtein;
 
-sub distance is export ($s, *@targets) {
-    my $n = $s.chars;
+sub distance is export ($s, $t) {
+    return 0 if $s eq $t;
+
     my @result;
 
-    for @targets -> $t {
-        if ($s eq $t) { @result.push: 0; next }
-        my @d;
-        my $cost = 0;
+    my $n = $s.chars;
+    my $m = $t.chars;
+    my @d;    
 
-        my $m = $t.chars;
-        unless $n { @result.push: $m; next }
-        unless $m { @result.push: $n; next }
-
-
-        @d[0] = [];         # XXX Work around rakudo bug
-        @d[0][0] = 0;
-        for 1..$n -> $i {
-            @d[$i] = [];    # XXX Work around rakudo bug 
-            if ($i != $n && $s.substr($i) eq $t.substr($i)) { @result.push: $i; next; }
-            @d[$i][0] = $i;
+    # Work around rakudo bug 
+    for 0..$n -> $i {
+        for 0..$m -> $j {
+            @d[$i] = [];
+            @d[$i][$j] = 0;
         }
-
-        for 1..$m -> $j {
-            if ($j != $m && $s.substr($j) eq $t.substr($j)) { @result.push: $j; next; }
-            @d[0][$j] = $j;
-        }
-
-        for 1..$n -> $i {
-            my $s-i = $s.substr($i-1,1);
-            for 1..$m -> $j {
-                my $k = $s-i eq $t.substr($j-1,1) ?? 0 !! 1;
-                @d[$i][$j] = (@d[$i-1][$j]+1, @d[$i][$j-1]+1, @d[$i-1][$j-1] + $k).min;
-            }
-        }
-
-        @result.push: @d[$n][$m];
     }
-    return @result;
+
+    @d[0][0] = 0;
+    for 1..$n -> $i {
+        @d[$i][0] = $i;
+        return $i if $i != $n && $s.substr($i) eq $t.substr($i);
+    }
+
+    for 1..$m -> $j {
+        @d[0][$j] = $j;
+        return $j if $j != $m && $s.substr($j) eq $t.substr($j);
+    }
+
+    for 1..$n -> $i {
+        my $s-i = $s.substr($i-1,1);
+        for 1..$m -> $j {
+            my $k = $s-i eq $t.substr($j-1,1) ?? 0 !! 1;
+            @d[$i][$j] = (@d[$i-1][$j]+1, @d[$i][$j-1]+1, @d[$i-1][$j-1] + $k).min;
+        }
+    }
+    @result.push: @d[$n][$m];
+
+    return @result[0];
 }
 
 
